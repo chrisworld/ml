b67c705c7106c7fd6dfd187694d31cc78fe05a2d
Christian Walter <christian.walter@student.tugraz.at>
2017-03-27 14:48:03 +0300

    sffs

--- classify.m
+++ classify.m
@@ -16,14 +16,13 @@ function classify()
 %% - Load the data
 close all
 clear all
-clc
 load training_data
 
 %trainingData = standardize(trainingData);
 
 %% - Split the data to training and validation sets.
 N = size(trainingData,1);
-N = 1000;    %-%
+N = 2000;    %-%
 selection = 1:N;
 training_data = trainingData(selection(1:floor(2*N/3)), :);
 training_class = class_trainingData(selection(1:floor(2*N/3)), :);
@@ -119,37 +118,43 @@ end
 %            See MATLAB help for details on these.
 %%
 function parameters = trainClassifier( samples, classes )
-%% Comments
-% Hint 1: If you wish to avoid overtraining, you could partition the
-% training samples to a actual training data set and a testing data set
-% that is used for a stopping/overtraining criterion.
+    %% Comments
+    % Hint 1: If you wish to avoid overtraining, you could partition the
+    % training samples to a actual training data set and a testing data set
+    % that is used for a stopping/overtraining criterion.
 
 
-%% Whitening with eigenvalue decompensation
-[A, B] = eig(cov(samples));
-samples_white = whitening(samples, A, B);
+    %% Whitening with eigenvalue decompensation
+    [A, B] = eig(cov(samples));
+    samples_white = whitening(samples, A, B);
 
-%% Train feature vector
-k = 3;
-num_features = size(samples_white,2);
-fvector = zeros(num_features,1);
-best_result = 0;
-for in = 1:num_features
-    [best_result_add, best_feature_add] = ...
-        forwardsearch(samples_white, classes, fvector, k);
-    % Update the feature vector  
-    fvector(best_feature_add) = 1;
-  
-    % Save best result
-    if(best_result < best_result_add)
-        best_result = best_result_add;
-        best_fvector = fvector;
+    %% Train feature vector
+    k = 3;
+    num_features = size(samples_white,2);
+    fvector = zeros(num_features,1);
+    best_result = 0;
+    %{
+    % Forward
+    for in = 1:num_features
+        [best_result_add, best_feature_add] = ...
+            forwardsearch(samples_white, classes, fvector, k);   
+        % Update the feature vector  
+        fvector(best_feature_add) = 1;
+        % Save best result
+        if(best_result < best_result_add)
+            best_result = best_result_add;
+            best_fvector = fvector;
+        end
     end
-end
-
-parameters = struct('training_samples_white', samples_white, ...
-    'white_A', A, 'white_B', B,'training_class', classes,...
-    'best_fvector', best_fvector, 'k', k);
+    %}
+    
+    [best_result, best_fvector] = ...
+        sffs(samples_white, classes, fvector, k);
+    % best_fvector
+    
+    parameters = struct('training_samples_white', samples_white, ...
+        'white_A', A, 'white_B', B,'training_class', classes,...
+        'best_fvector', best_fvector, 'k', k);
 
 end
 
@@ -182,14 +187,14 @@ end
 %            vector in the previous function.
 %%
 function results = evaluateClassifier( samples, parameters )
-%% Whitening
-samples_white = whitening(samples, ...
-    parameters.white_A, parameters.white_B);
+    %% Whitening
+    samples_white = whitening(samples, ...
+        parameters.white_A, parameters.white_B);
 
-%% Classification
-results = knnclass(samples_white, parameters.training_samples_white, ...
-    parameters.best_fvector, parameters.training_class, parameters.k);
-
+    %% Classification
+    results = knnclass(samples_white, ...
+        parameters.training_samples_white, ...
+        parameters.best_fvector, parameters.training_class, parameters.k);
 end
 
 
@@ -249,7 +254,104 @@ function [best, feature] = forwardsearch(data, data_c, fvector, k)
     end
 end
 
-%% Whitening
+%% SFFS
+function [res_vector, best_fset] = sffs(data, data_c, fvector, k)
+    %% SFFS Init
+    best_result = 0;
+    n_features = 1;
+    max_n_features = length(fvector);
+    res_vector = zeros(1, max_n_features); 
+    % search_direction: forwards when 0, backwards when 1
+    search_direction = 0; 
+
+    %% loop
+    while(n_features <= max_n_features)
+        % Inclusion
+        [best_result_add, best_feature_add] = ...
+            findbest(data, data_c, fvector, search_direction, k); 
+        % update
+        fvector(best_feature_add) = 1;
+        if(best_result < best_result_add)
+            best_result = best_result_add;
+            best_fset = fvector;
+        end
+        if(best_result_add > res_vector(n_features))
+            res_vector(n_features) = best_result_add;
+        end
+        disp([res_vector(n_features), n_features])
+
+        % Exclusion
+        search_direction = 1;
+        while search_direction
+            if(n_features > 2)
+                % remove one of the features
+                [best_result_rem, best_feature_rem] = ...
+                    findbest(data, data_c, fvector, search_direction, k);
+                % If better than before, step backwards and update results
+                % otherwise we will go to the inclusion step
+                if(best_result_rem > res_vector(n_features - 1))
+                    fvector(best_feature_rem) = 0;
+                    n_features = n_features - 1;
+                    if(best_result < best_result_rem)
+                        best_result = best_result_rem;
+                        best_fset = fvector;
+                    end
+                    res_vector(n_features) = best_result_rem;
+                    disp([res_vector(n_features), n_features])
+                else
+                    search_direction = 0;
+                end
+            else
+                % In the case when the number of selected features is 
+                % less than 2, we will go back to the inclusion step
+                search_direction = 0;
+            end
+        end
+        n_features = n_features+1;
+    end
+end
+
+%% find best feature vector with knn
+function [best, feature] = findbest(data, data_c, fvector, direction,k)
+    num_samples = length(data);
+    best = 0;
+    feature = 0;
+    if(direction == 0)
+        for in = 1:length(fvector)
+            if (fvector(in) == 0)
+                fvector(in) = 1;
+                % Classify using k-NN
+                predictedLabels = knnclass(data, data, fvector, data_c, k);
+                % the number of correct predictions
+                correct = sum(predictedLabels == data_c); 
+                result = correct/num_samples; % accuracy
+                if(result > best)
+                    best = result; 
+                    feature = in; 
+                end
+                fvector(in) = 0;
+            end
+        end
+    else
+        for in = 1:length(fvector)
+            if (fvector(in) == 1)
+                fvector(in) = 0;
+                % Classify using k-NN
+                predictedLabels = knnclass(data, data, fvector, data_c, k);
+                % the number of correct predictions
+                correct = sum(predictedLabels == data_c); 
+                result = correct/num_samples; % accuracy
+                if(result > best)
+                    best = result; 
+                    feature = in; 
+                end
+                fvector(in) = 1;
+            end
+        end
+    end
+end
+
+%% Whitening with eigenvalue decompensation
 function [feat_out] = whitening(feat_in, A, B)
 % eigenvalue decompensation
 feat_out = (sqrt(inv(B)) * A' * feat_in')';
